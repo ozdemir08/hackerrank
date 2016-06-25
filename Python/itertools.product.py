a368572d932da28c95793128771c97361e146cd7
mehmet <mehmetozdemir94@gmail.com>
2016-06-25 17:22:40 +0300

    itertools.product() is solved

---
+++ Python/itertools.product.py
@@ -0,0 +1,10 @@
+from itertools import product
+
+#print (list(product([1,2,3],repeat = 3)))
+# print (list(product([1,2,3],[3,4])))
+# A = [[1,2,3],[3,4,5],[6,7,8]]
+# print (list(product(*A)))
+# print(*A)
+
+print( *list(product(list(map(int,input().split())), list(map(int, input().split())))))
+
