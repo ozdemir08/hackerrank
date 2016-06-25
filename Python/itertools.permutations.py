4878f8ffdf87063058d554c5ae35719559c87652
mehmet <mehmetozdemir94@gmail.com>
2016-06-25 18:01:57 +0300

    itertools.combinayions() is solved

--- Python/itertools.permutations.py
+++ Python/itertools.permutations.py
@@ -1,11 +1,16 @@
-from itertools import permutations
+from itertools import combinations
 
-S, K  = input().split()
-list = list(permutations(S, int(K)))
-list.sort()
+S, K = input().split()
+K = int(K)
 
-for i in list:
-    s = ''
-    for j in i:
-        s += j
-    print(s)
+for a in range(1,K+1) :
+    l = list(combinations(S, a))
+    resultList = []
+    l.sort()
+
+    for i in l:
+        s = ''
+        for j in i:
+            s += j
+        resultList.append(''.join(sorted(s)))
+    print('\n'.join(sorted(resultList)))
