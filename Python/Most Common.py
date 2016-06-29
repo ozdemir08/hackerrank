c96ef5ae08ec198f832a9eeeb2d2757dda9e75e8
mehmet <mehmetozdemir94@gmail.com>
2016-06-29 17:13:14 +0300

    Most Common is solved.

--- Python/Most Common.py
+++ Python/Most Common.py
@@ -1,19 +1,7 @@
 from collections import Counter
-from operator import itemgetter
-
-
-def cmp(a, b):
-    if counter[a] > counter[b]:
-        return -1
-    if counter[a] < counter[b]:
-        return 1
-    if a < b:
-        return -1
-    return 1
-
-counter = Counter(input())
-
-print(sorted(counter, key=str.lower))
+#.items -> elements in tuple format
+# key=lambda x: (-x[1], x[0]))[0:3] sort in ascending order according to second one and descending order acc. to first one
+[print(x[0], x[1]) for x in sorted(Counter(input()).items(), key=lambda x: (-x[1], x[0]))[0:3]]
 
 # for t in most_commons:
 #     print(t[0],t[1])
